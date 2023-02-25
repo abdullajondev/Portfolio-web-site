97dede52cf75e9a8408192a44c632eb9a3cf1ee2
“Vitali <abdullajondev@gmail.com>
2023-02-25 11:30:16 +0500

    Day 22 Add images , app description and url

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 
 st.title("Home", )
 
-col1, col2 = st.columns(2)
+col1,space_col, col2 = st.columns([1.5, 0.5, 1.5])
 
 with col1:
     st.image("images/myphoto.Jpg", width=500)
@@ -26,14 +26,21 @@ Below you can find some of the app I have built in Python. Feel free to contact 
 """
 st.write(content1)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.info(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code:]({row['url']})")
+
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.info(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source code:]({row['url']})")
