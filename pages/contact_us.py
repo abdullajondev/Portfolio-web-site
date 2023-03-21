4798b83433c1881e756e1c3c5bdcd5fbcec32e93
“Vitali <abdullajondev@gmail.com>
2023-03-21 11:20:36 +0500

    send email code added

--- pages/contact_us.py
+++ pages/contact_us.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.header("Contact Me")
+
+with st.form(key="email forms"):
+    user_email = st.text_input("Your email adress")
+    message = st.text_area("your message")
+    button = st.form_submit_button()
